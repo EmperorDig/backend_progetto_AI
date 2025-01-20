21ba4c3f7dad8a3ceae20ac5da3daf04bc2e9316
EmperorDig <150080121+EmperorDig@users.noreply.github.com>
2025-01-20 20:13:53 +0100

    creazione del modello per il database

--- backend_project/audio_management/models.py
+++ backend_project/audio_management/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Audio(models.Model):
+    name = models.CharField(max_length=30)
+    audio_file = models.FileField(upload_to='audio_files/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.audio_file.name
