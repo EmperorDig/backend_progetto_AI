187cb688c1a332b497dde32111dcf62b0335bf97
EmperorDig <150080121+EmperorDig@users.noreply.github.com>
2025-02-18 14:19:29 +0100

    aggiornamento ping

--- backend_project/audio_management/urls.py
+++ backend_project/audio_management/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('audio-list/', AudioListView, name='audio-list'),
     path('audio-create/', AudioCreateView, name='audio-create'),
     path('audio-getdata/<int:audio_id>/', GetAudioData, name='audio-getdata'),
-    path('/', PingView, name='ping-view')
+    path('ping/', PingView, name='ping-view')
 ]
